044b50bd7e1be9ec3c31332080b82fe3b3dce467
GauravRoy-20 <gauravroy20.gr@gmail.com>
2025-02-02 22:29:12 +0530

    test husky

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 function welcome(name: string) {
-  console.log(`welcome ${name} to fullstack engineering course`);
+    //   console.log(`welcome ${name} to fullstack engineering course`);
+    return `welcome ${name} to fullstack engineering course`;
 }
 
 welcome("Gaurav");
