047ae53931ed6f0c39c5eda25cdd0c9ce56e1b8e
GauravRoy-20 <gauravroy20.gr@gmail.com>
2025-02-03 22:04:45 +0530

    added express server

---
+++ src/config/logger.ts
@@ -0,0 +1,14 @@
+import winston from 'winston'
+
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: { serviceName: 'auth-service' },
+    transports: [
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.json(),
+        }),
+    ],
+})
+
+export default logger
