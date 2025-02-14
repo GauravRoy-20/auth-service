2606ebc322bb578b9f2f07d0b087f7a342eb972d
GauravRoy-20 <gauravroy20.gr@gmail.com>
2025-02-14 14:32:14 +0530

    added test for unique email

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,18 +2,31 @@ import { Repository } from 'typeorm'
 import { User } from '../entity/User'
 import { UserData } from '../types'
 import createHttpError from 'http-errors'
+import bcrypt from 'bcrypt'
 import { Roles } from '../constants'
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
+        const user = await this.userRepository.findOne({
+            where: { email: email },
+        })
+
+        if (user) {
+            const err = createHttpError(400, 'Email is already exists!')
+            throw err
+        }
+
+        // hash password
+        const saltRounds = 10
+        const hashPassword = await bcrypt.hash(password, saltRounds)
         try {
             return await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashPassword,
                 role: Roles.CUSTOMER,
             })
         } catch {
--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -116,6 +116,54 @@ describe('POST /auth/register', () => {
             expect(users[0]).toHaveProperty('role')
             expect(users[0].role).toBe(Roles.CUSTOMER)
         })
+        it('should store the hashed password in the database', async () => {
+            // arrange
+
+            const userData = {
+                firstName: 'Gaurav',
+                lastName: 'Roy',
+                email: 'gaurav@mail.com',
+                password: 'test@123',
+            }
+
+            // act
+
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            await request(app).post('/auth/register').send(userData)
+
+            // assert
+            const userRepository = connection.getRepository(User)
+            const user = await userRepository.find()
+            expect(user[0].password).not.toBe(userData.password)
+            expect(user[0].password).toHaveLength(60)
+            expect(user[0].password).toMatch(/^\$2b\$\d+\$/)
+        })
+
+        it('should return 400 status code if email is already exists', async () => {
+            // arrange
+
+            const userData = {
+                firstName: 'Gaurav',
+                lastName: 'Roy',
+                email: 'gaurav@mail.com',
+                password: 'test@123',
+            }
+
+            const userRepository = connection.getRepository(User)
+            await userRepository.save({ ...userData, role: Roles.CUSTOMER })
+
+            // act
+
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+            const users = await userRepository.find()
+
+            // assert
+            expect(response.statusCode).toBe(400)
+            expect(users).toHaveLength(1)
+        })
     })
     describe('Fields are missing', () => {})
 })
